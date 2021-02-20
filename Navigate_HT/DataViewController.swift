7bd658411f8aea7275d36aa6444ce150953af66f
Hayati Tastan <haytastan@gmail.com>
2021-02-21 00:52:49 +0300

    initial commit by Hayati TASTAN

---
+++ Navigate_HT/DataViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DataViewController.swift
+//  Navigate_HT
+//
+//  Created by Hayati TASTAN on 20.02.2021.
+//  Copyright © 2021 Hayati TASTAN. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
---
+++ Navigate_HTTests/Navigate_HTTests.swift
@@ -0,0 +1,34 @@
+//
+//  Navigate_HTTests.swift
+//  Navigate_HTTests
+//
+//  Created by Hayati TASTAN on 20.02.2021.
+//  Copyright © 2021 Hayati TASTAN. All rights reserved.
+//
+
+import XCTest
+@testable import Navigate_HT
+
+class Navigate_HTTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
